27227d864650de78a824a692f6bd692ee8499748
aminebeji <aminebeji007@gmail.com>
2021-07-11 14:17:42 +0100

    heroku commit

--- FaceBookBackEnd/server.js
+++ FaceBookBackEnd/server.js
@@ -1,12 +1,21 @@
-const express = require ("express")
-const app = express ()
-require("./Config/DataBase")
-const AuthRoute = require ("./routers/AuthRoute")
-const UserRoute = require ("./routers/UserRoute")
-const port = process.env.PORT || 4000
-app.use(express.json())
-app.use(AuthRoute)
-app.use(UserRoute)
-app.listen(port, ()=>{
-    console.log("server runninig")
-})
+const express = require("express");
+const app = express();
+require("./Config/DataBase");
+
+const AuthRoute = require("./routers/AuthRoute");
+const UserRoute = require("./routers/UserRoute");
+
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(AuthRoute);
+app.use(UserRoute);
+
+app.use(express.static(path.join(__dirname, "../build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build"));
+});
+
+app.listen(port, () => {
+  console.log("server runninig");
+});
